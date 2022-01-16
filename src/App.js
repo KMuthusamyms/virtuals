aed1a10f28bd04dc1554580dfd7e0280a2cbf76f
KMuthusamyms <k.muthusamyms@gmail.com>
2022-01-16 20:48:46 +0530

    enhancement: add path and points

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import {
   TimeIntervalCollection,
   TimeInterval,
   PathGraphics,
-  VelocityOrientationProperty
+  VelocityOrientationProperty,
+  PolylineGlowMaterialProperty,
 } from "cesium";
 import { Viewer } from "resium";
 import { flightData } from "./utils/consts";
@@ -22,7 +23,6 @@ function App() {
   const start = JulianDate.fromIso8601(new Date().toISOString());
   const stop = JulianDate.addSeconds(start, totalSeconds, new JulianDate());
   useEffect(() => {
-    console.log(ref);
     const viewer = ref?.current?.cesiumElement;
     if (viewer) {
       viewer.scene.primitives.add(createOsmBuildings());
@@ -37,31 +37,61 @@ function App() {
 
       for (let i = 0; i < flightData.length; i++) {
         const dataPoint = flightData[i];
-        const time = JulianDate.addSeconds(start, i * timeStepInSeconds, new JulianDate());
-        const position = Cartesian3.fromDegrees(dataPoint.longitude, dataPoint.latitude, dataPoint.height);
+        const time = JulianDate.addSeconds(
+          start,
+          i * timeStepInSeconds,
+          new JulianDate()
+        );
+        const position = Cartesian3.fromDegrees(
+          dataPoint.longitude,
+          dataPoint.latitude,
+          dataPoint.height
+        );
         positionProperty.addSample(time, position);
-  
+
         viewer.entities.add({
           description: `Location: (${dataPoint.longitude}, ${dataPoint.latitude}, ${dataPoint.height})`,
           position: position,
-          point: { pixelSize: 10, color: Color.RED }
+          point: {
+            pixelSize: 10,
+            color: Color.TRANSPARENT,
+            outlineColor: Color.RED,
+            outlineWidth: 2,
+          },
         });
       }
 
       const airplaneEntity = viewer.entities.add({
-        availability: new TimeIntervalCollection([ new TimeInterval({ start: start, stop: stop }) ]),
+        availability: new TimeIntervalCollection([
+          new TimeInterval({ start: start, stop: stop }),
+        ]),
         position: positionProperty,
-        model: { uri: "https://s3.amazonaws.com/cesiumjs/downloads/Cesium_Air.glb" },
-        orientation: new VelocityOrientationProperty(positionProperty),    
-        path: new PathGraphics({ width: 3 })
+        model: {
+          uri: "https://s3.amazonaws.com/cesiumjs/downloads/Cesium_Air.glb",
+        },
+        orientation: new VelocityOrientationProperty(positionProperty),
+        path: new PathGraphics({
+          resolution: 4,
+          material: new PolylineGlowMaterialProperty({
+            glowPower: 0.5,
+            color: Color.YELLOW,
+          }),
+          width: 3,
+        }),
       });
-      
+
       viewer.trackedEntity = airplaneEntity;
     }
   }, [start, stop]);
 
   return (
-    <Viewer ref={ref} full terrainProvider={createWorldTerrain()} />
+    <Viewer
+      ref={ref}
+      homeButton={false}
+      sceneModePicker={false}
+      full
+      terrainProvider={createWorldTerrain()}
+    />
   );
 }
 
